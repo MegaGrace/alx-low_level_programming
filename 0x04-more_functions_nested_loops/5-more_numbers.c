49ca64cc52a0ad435c1ce47edea7d4c090f58091
MegaGrace <danmarley2000@yahoo.com>
2021-09-04 05:09:30 +0000

    0x04-more_functions_nested_loop modified

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,19 +7,19 @@
 
 void more_numbers(void)
 {
-int n, i;
+int x, y;
 
-for (i = 0; i <= 10; i++)
+for (x = 0; x < 10; x++)
 {
-for (n = 0; n <= 14; n++)
+for (y = 0; y <= 14; y++)
 {
-if (n >= 10)
+if (y > 10)
 {
-_putchar(n / 10 + '0');
+_putchar((y / 10) + '0');
 }
 else
 {
-_putchar(n % 10 + '0');
+_putchar((y % 10) + '0');
 }
 }
 _putchar('\n');

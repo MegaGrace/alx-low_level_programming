d950a8addc1fea2287e105741344cc7aacff8b36
MegaGrace <danmarley2000@yahoo.com>
2021-09-06 03:13:33 +0000

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
 /**
- *main - Entry point
+ *reset_to_98 - Entry point
  *@n: integer
  *Return: Always 0
  */
 
 void reset_to_98(int *n)
 {
-	int v = 98;
-	*n = &v;
+	*n = 98;
 }

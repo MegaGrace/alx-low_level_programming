24b2137865fdcfaba68c3e8a5c99c3c086a0517c
Daniel Nai <danmarley2000@yahoo.com>
2021-09-06 16:53:09 +0000

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *print_rev - prints reverse
+ *@s: pointer variable
+ *
+ *Return: Always 0
+ */
+
+void print_rev(char *s)
+{
+	int c = 0;
+
+	while (s[c] != '\0')
+	{
+		c++;
+	}
+
+	for (c -= 1; c >= 0; c--)
+	{
+		_putchar(s[c]);
+	}
+	_putchar('\n');
+}

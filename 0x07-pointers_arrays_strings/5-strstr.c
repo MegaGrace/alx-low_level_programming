34387266bfb0a45fb1e7e461d7cbd9800cfe768f
Daniel Nai <danmarley2000@yahoo.com>
2021-09-13 17:04:16 +0000

    0x07-pointers_arrays_strings

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ *5-strstr.c - locates a function string
+ *@haystack: the string to be searched
+ *@needle: the substring to be located
+ *
+ *Return: pointer or null
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+
+	if (*needle == 0)
+		return (haystack);
+
+	while (*haystack)
+	{
+		i = 0;
+
+		if (haystack[i] == needle[i])
+		{
+			do {
+				if (needle[i + 1] == '\0')
+					return (haystack);
+
+				i++;
+			} while (haystack[i] == needle[i]);
+		}
+		haystack++;
+	}
+	return ('\0');
+}

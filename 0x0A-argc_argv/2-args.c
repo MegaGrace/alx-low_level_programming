740b1e244c50864f29b7f02c6fedb557fecc98d7
MegaGrace <danmarley2000@yahoo.com>
2021-09-19 23:16:17 +0000

    0x0A-argc_argv

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints all the arguments it receives
+ * @argc: argument count
+ * @argv: array of string  arguments
+ * Return: Always 0 if successful
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 1; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}

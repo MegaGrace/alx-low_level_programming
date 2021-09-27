cc9a643fbf5c846f522d4e7a52493fc1dd7c4425
Daniel Nai <danmarley2000@yahoo.com>
2021-09-27 18:04:20 +0000

    struct modified

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,11 @@
 #ifndef FILE_DOG
 #define FILE_DOG
+/**
+ * struct dog - structure dog
+ * @name: atribute pointer to char name's dog
+ * @age: atribute float age's dog
+ * @owner: atribute pointer to char owner's dog
+ */
 
 struct dog
 {

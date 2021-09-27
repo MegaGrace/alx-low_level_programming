8ffe0f868423e811b0f35785d20d9476a8922b59
Daniel Nai <danmarley2000@yahoo.com>
2021-09-27 18:00:14 +0000

    struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,13 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef FILE_DOG
+#define FILE_DOG
 
 struct dog
 {
 	char *name;
 	float age;
-	char *owner:
+	char *owner;
 };
+
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
